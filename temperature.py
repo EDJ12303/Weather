56a18be313f9440b97280297185503e533790213
EDJ12303 <erin.jeffery@comcast.net>
2016-04-18 21:18:56 -0400

    first

--- temperature.py
+++ temperature.py
@@ -7,8 +7,6 @@ Created on Wed Apr 06 16:49:04 2016
 import datetime
 import requests
 import pandas as pd
-from pandas.io.json import json_normalize
-from dateutil.parser import parse
 import sqlite3 as lite
 
 cities = {  "Austin": '30.303936,-97.754355',
@@ -16,7 +14,7 @@ cities = {  "Austin": '30.303936,-97.754355',
             "Chicago": '41.837551,-87.681844',
             "Cleveland": '41.478462,-81.679435',
             "Denver": '39.761850,-104.881105'
-        }
+          }
         
 con = lite.connect('weather.db')
 cur = con.cursor()
@@ -33,29 +31,37 @@ print end_date.strftime('%Y-%m-%dT%H:%M:%S')
 
 cities.keys()
 with con:
-    cur.execute('CREATE TABLE daily_maxtemp (day_of_reading INT, Austin REAL, Boston REAL, Chicago REAL, Cleveland REAL, Denver REAL);')
+    cur.execute('CREATE TABLE daily_maxtemp (day_of_reading INT, Austin REAL, 
+    Boston REAL, Chicago REAL, Cleveland REAL, Denver REAL);')
 with con:
     while start_date < end_date:
-        cur.execute("INSERT INTO daily_maxtemp(day_of_reading) VALUES (?)", (int(start_date.strftime('%Y%m%d')),))
+        cur.execute("INSERT INTO daily_maxtemp(day_of_reading) VALUES (?)", 
+                    (int(start_date.strftime('%Y%m%d')),))
         start_date += datetime.timedelta(days=1)
 
 #loop through the cities and query the API
 for k,v in cities.iteritems():
-    start_date = end_date - datetime.timedelta(days=30) #reset value each time through the loop 
+    start_date = end_date - datetime.timedelta(days=30) #reset value each time
+    through the loop 
     while start_date < end_date:
 
         #query for the value
-        r = requests.get("https://api.forecast.io/forecast/8e2f0f9e97727516429316ac1aaec3ad/"+ v + "," + str(start_date.strftime('%Y-%m-%dT%H:%M:%S')))
+        r = requests.get
+        ("https://api.forecast.io/forecast/8e2f0f9e97727516429316ac1aaec3ad/"
+        + v + "," + str(start_date.strftime('%Y-%m-%dT%H:%M:%S')))
         
         with con:
             #insert the temperature max to the database
-            cur.execute('UPDATE daily_maxtemp SET ' + k + ' = ' + str(r.json()['daily']['data'][0]['temperatureMax']) + ' WHERE day_of_reading = ' + start_date.strftime('%Y%m%d'))
+            cur.execute('UPDATE daily_maxtemp SET ' + k + ' = '
+            + str(r.json()['daily']['data'][0]['temperatureMax']) 
+            + ' WHERE day_of_reading = ' + start_date.strftime('%Y%m%d'))
             
         #increment start_date to the next day 
         start_date += datetime.timedelta(days=1) 
         
 
-df = pd.read_sql_query("SELECT * FROM daily_maxtemp",con,index_col='day_of_reading')
+df = pd.read_sql_query
+("SELECT * FROM daily_maxtemp",con,index_col='day_of_reading')
 #mean temp for each city
 print (df)
 
